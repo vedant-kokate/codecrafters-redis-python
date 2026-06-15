3e2febcca79ae16142d12d23e2c3fe21781bfbe8
vedant-kokate <vedant.kokate@somaiya.edu>
2026-06-15 09:46:55 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,22 +3,14 @@ import threading
 
 def handle(conn):
     while data := conn.recv(1024):
-        conn.sendall(b"+PONG\r\n")
+        conn.sendall(b"+PONG\r\n") 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment the code below to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    
-
-    
-    while True:
-        connection, _ = server_socket.accept() 
-        threading.Thread(target=handle, args=(connection,)).start()
-        
+    with socket.create_server(("localhost", 6379), reuse_port=True) as server:
+        while True:
+            connection, _ = server.accept() 
+            threading.Thread(target=handle, args=(connection,)).start()
+            
 
 
 if __name__ == "__main__":
