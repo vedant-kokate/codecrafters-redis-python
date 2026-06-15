6f64f552f1e6d5217bf4f837d0249857722bb7fc
vedant-kokate <vedant.kokate@somaiya.edu>
2026-06-15 09:30:46 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,8 +9,6 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-    
-    conn, addr = server_socket.accept()
 
     print("Client connected")
 
