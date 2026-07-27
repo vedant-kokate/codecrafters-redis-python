c80e97cf60004c71cf7c244c4cd340f0fab15938
vedant-kokate <vedant.kokate@somaiya.edu>
2026-07-27 18:31:36 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -103,7 +103,7 @@ def handle_lpop(conn, parts):
 
 def handle_blpop(conn, parts):
     key = parts[4]
-    timeout = int(parts[6])
+    timeout =  None if int(parts[6]) == 0 else int(parts[6])
     cond = get_condition(key)
 
     with cond:
