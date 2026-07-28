54e9e1d626d9862062feb089e554b0db6008a7e1
vedant-kokate <vedant.kokate@somaiya.edu>
2026-07-28 21:47:03 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -310,9 +310,10 @@ def handle_exec(conn, transaction):
     for parts in transaction["queue"]:
         response = parse_command(conn, "\r\n".join(parts) + "\r\n", transaction)
         responses.append(response)
+    if not responses:
+        return b"*0\r\n"
+    return "\r\n".join(responses).encode()
     
-    return b"".join(responses)
-
 def parse_command(conn, data, transaction):
     parts = data.decode().split("\r\n")
     command = parts[2].upper()
