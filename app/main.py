3524768221b75254c48ed6563c17d2091cb22e2f
vedant-kokate <vedant.kokate@somaiya.edu>
2026-08-29 15:30:06 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -391,7 +391,7 @@ def handle_info(conn, parts):
 def handle_psync(conn, parts):
     rdb = base64.b64decode(EMPTY_RBD_FILE_64)
     return (b"+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n"
-            + bulk(rdb.decode("latin-1")).encode("latin-1")
+            + + bulk(rdb.decode("latin-1"))
     )
     
 def parse_command(conn, data, transaction):
