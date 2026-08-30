0ad4de4422b06e1d7c1b6fef4a51bfe80240e9b5
vedant-kokate <vedant.kokate@somaiya.edu>
2026-08-30 16:43:32 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -944,7 +944,7 @@ def parse_command(conn, data, transaction, is_master=False):
     parts = data.decode().split("\r\n")
     command = parts[2].upper()
     if command != "AUTH" and not pre_auth(conn, parts):
-        return b"NOAUTH Authentication required."
+        return b"-NOAUTH Authentication required.\r\n", False
         
     if transaction["in_multi"] and command not in ("EXEC", "DISCARD", "MULTI", "WATCH", "UNWATCH"):
         transaction["queue"].append(parts)
