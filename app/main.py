b2e6a7db1b28aecda3bc7175a8b7b9c4b0e079f2
vedant-kokate <vedant.kokate@somaiya.edu>
2026-09-17 22:54:16 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -936,8 +936,9 @@ def handle_getbit(parts):
             return integer(0)
         val = val.encode("utf-8")[byte_index]
         print(f"{bit_index}: {bin(val)}")
-    bit_value = (val >> (7 - bit_index)) & 1
-    return integer(bit_value)
+        bit_value = (val >> (7 - bit_index)) & 1
+        return integer(bit_value)
+    return integer((val >> offset) & 1)
  
 def get_aof_file_path(manifest_path):
     manifest = manifest_path.read_text().splitlines()
