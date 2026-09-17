6c3dfc154371c6708bf9e43ebcc29cfa630bb55c
vedant-kokate <vedant.kokate@somaiya.edu>
2026-09-17 22:48:08 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -935,7 +935,7 @@ def handle_getbit(parts):
         if byte_index >= len(val):
             return integer(0)
         val = val.encode("utf-8")[byte_index]
-        print(f"Byte value at index {byte_index}: {val}")
+        print(f"Byte value at index {byte_index}: {bin(val)}")
     bit_value = (1 << bit_index) & val
     return integer(bit_value)
  
